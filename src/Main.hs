7c8e0d04b42568fc0190da4347305c2fca7eb3f1
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2014-06-22 16:16:19 +0200

    Implemented Widget PoC.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, ScopedTypeVariables, NoMonomorphismRestriction #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 module Main(main) where
 
 import Reactive.Banana.Monitors
@@ -12,6 +13,8 @@ import qualified Reactive.Banana.Monitors.Tick as T
 
 import Control.Concurrent.Suspend (suspend, sDelay, hDelay)
 import Control.Concurrent.Timer
+
+import Data.Monoid
 
 import Reactive.Banana
 import Reactive.Banana.Frameworks
@@ -33,16 +36,36 @@ eventLoop tick cpu = do
     monitors <- sequence [initMonitor cpu, initMonitor tick]
     repeatedTimer (updateMonitors monitors) (sDelay 1)
     loop
-  where loop = do suspend (hDelay 24) >> loop
+  where loop = suspend (hDelay 24) >> loop
 
 
-
-setupNetwork :: Frameworks t => SourceOf Tick -> SourceOf CpuMonitor -> Moment t ()
+setupNetwork :: Frameworks t
+              => SourceOf Tick -> SourceOf CpuMonitor -> Moment t ()
 setupNetwork tickSrc cpuSrc = do
   cpu  <- fromMonitorSource cpuSrc
-  etick <- tick <$> fromMonitorSource tickSrc
+  tick <- fromMonitorSource tickSrc
 
-  let boutput = cpuFormat <$> Cpu.busy cpu
-      cpuFormat v = concat ["CPU: ", show . round $ v*100, "%"]
-  reactimate (putStrLn <$> boutput <@ etick)
-  
+  reactimateBar tick (cpuWidget cpu)
+
+reactimateBar :: Frameworks t => Tick t -> Widget t -> Moment t ()
+reactimateBar tick widget =
+  reactimate (putStrLn <$> unWidget widget <@ T.tick tick)
+
+newtype Widget t = Widget { unWidget :: Behavior t String }
+
+instance Monoid (Widget t) where
+  mempty = Widget $ pure ""
+  Widget b1 `mappend` Widget b2 = Widget $ (++) <$> b1 <*> b2
+
+label :: String -> Widget t
+label = Widget . pure
+
+string :: Behavior t String -> Widget t
+string = Widget
+
+wshow :: Show a => Behavior t a -> Widget t
+wshow = Widget . fmap show
+
+cpuWidget :: CpuMonitor t -> Widget t
+cpuWidget cpu = let percent = round . (*100) <$> Cpu.busy cpu
+                in label "CPU: " <> wshow percent <> label "%"
